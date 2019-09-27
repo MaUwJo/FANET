b70e8ec5d037f01a55705978997d156f7581dce3
mreinart <manfred@reinart.de>
2019-09-27 20:21:26 +0200

    Update main.c
    sql_login

--- Groundstation/V0_2/main.c
+++ Groundstation/V0_2/main.c
@@ -1,24 +1,24 @@
 /*
  * main.c
- * 
+ *
  * Copyright 2019  <pi@RPi3B_FANET_2>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
 
 
@@ -46,7 +46,7 @@
 #include "fanet_t4_service.h"
 #include "fanet_routing.h"
 
-#include <sys/ioctl.h> 
+#include <sys/ioctl.h>
 
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
@@ -80,7 +80,7 @@ void system_data ()
 {
 	static sSystem	_system_data;
 	sRadioData		_radiodata;
-		
+
 	static int 		_yy;
 	static int		_rssi_sum_1min;
 	static float	_rssi_sum_15min;
@@ -88,7 +88,7 @@ void system_data ()
 	static int		_rssi_max_15min = -157;
 	static int  	_rssi_avg_1min_counter;
 	static int  	_rssi_avg_15min_counter;
-	
+
 	if (_yy == 500)			// 500 ms update rate
 	{
 		t = time(NULL);
@@ -97,10 +97,10 @@ void system_data ()
 		gettimeofday (&tv, NULL);
 		ptm = localtime (&tv.tv_sec);
 		milliseconds = tv.tv_usec / 1000;
-		strftime (time_string, sizeof (time_string), "%Y-%m-%d %H:%M:%S", ptm);	
+		strftime (time_string, sizeof (time_string), "%Y-%m-%d %H:%M:%S", ptm);
 
 		read_radio_data(&_radiodata);
-		
+
 		if ((_radiodata.rssi > -130) & (_radiodata.rssi < 0))		// Ignore RSSI values <-130dBm and > 0dBm
 		{
 			_rssi_sum_1min += _radiodata.rssi;
@@ -109,17 +109,17 @@ void system_data ()
 				_rssi_max_1min = _radiodata.rssi;
 		}
 
-        printf("|%s.%03ld| RSSI:%+4ddBm  RX Headers: %d \b\r", time_string, milliseconds, _radiodata.rssi, _radiodata.rx_headers);	
+        printf("|%s.%03ld| RSSI:%+4ddBm  RX Headers: %d \b\r", time_string, milliseconds, _radiodata.rssi, _radiodata.rx_headers);
 		fflush(stdout);
-	
+
 		if (!(t % 60) && _rssi_avg_1min_counter > 2)		// Modulo Divison: Volle Minute = kein Rest bei Disision durch 60 (1 min)
 		{
 			radio_info(&_system_data);						// Reads the tx/rx packets and tx time. After reading, values will be reset to 0.
-			
+
 			_system_data.timestamp_1min = t;
 			_system_data.rssi_avg_1min = (float)_rssi_sum_1min / (float)_rssi_avg_1min_counter;
 			_system_data.rssi_max_1min = _rssi_max_1min;
-			
+
 			printf ("\nSystem  1 min Data: %ld RSSI avg: %+7.2fdBm  RSSI max: %ddBm  RX packets: %d  TX packets: %d  TX time: %dms\n",
 				_system_data.timestamp_1min,
 				_system_data.rssi_avg_1min,
@@ -129,8 +129,8 @@ void system_data ()
 				_system_data.tx_time_1min);
 
 			write_system_data_1min (&_system_data);
-			//delete_old_data();	
-			
+			//delete_old_data();
+
 			_rssi_sum_15min += _system_data.rssi_avg_1min;
 			_rssi_avg_15min_counter++;
 			if (_rssi_max_1min > _rssi_max_15min)
@@ -143,15 +143,15 @@ void system_data ()
 			_rssi_sum_1min = 0;
 			_rssi_avg_1min_counter = 0;
 			_rssi_max_1min = -157;
-			
+
 			_system_data.rx_packets_1min = 0;
 			_system_data.tx_packets_1min = 0;
 			_system_data.tx_time_1min = 0;
-			
+
 			if (!(t % 900) && _rssi_avg_15min_counter > 2)		// Modulo Divison: Volle Minute = kein Rest bei Disision durch 900 (15 min)
 			{
 				_system_data.timestamp_15min = t;
-				
+
 				_system_data.rssi_avg_15min = _rssi_sum_15min /(float) _rssi_avg_15min_counter;
 				_system_data.rssi_max_15min = _rssi_max_15min;
 
@@ -163,12 +163,12 @@ void system_data ()
 					_system_data.tx_packets_15min,
 					_system_data.tx_time_15min);
 
-				write_system_data_15min (&_system_data);				
-				
+				write_system_data_15min (&_system_data);
+
 				_rssi_sum_15min = 0;
 				_rssi_avg_15min_counter = 0;
 				_rssi_max_15min = -157;
-				
+
 				_system_data.rx_packets_15min = 0;
 				_system_data.tx_packets_15min = 0;
 				_system_data.tx_time_15min = 0;
@@ -189,8 +189,8 @@ void receivepacket()
 
 	if(read_rx_data(&_rx_radio, &_radiodata))
     {
-		_fanet_mac.valid_bit = 1; 
-		
+		_fanet_mac.valid_bit = 1;
+
 		if (_radiodata.crc_err)
 			terminal_message_crc_err (0,0,&_radiodata, &_fanet_mac);
 
@@ -200,7 +200,7 @@ void receivepacket()
 
 		terminal_message_raw (0,0, &_radiodata, &_fanet_mac, &_rx_radio);
 
-					
+
 		if (_fanet_mac.valid_bit && !_radiodata.crc_err)
 		{
 			switch (_fanet_mac.type)
@@ -213,14 +213,14 @@ void receivepacket()
 				//default: terminal_mac_info (0,0,&_fanet_mac); printf ("\n");
 			}
 			if (_fanet_mac.ack)
-				send_ack (&_fanet_mac);			
+				send_ack (&_fanet_mac);
 		}
 		else
 		{
 			terminal_rf_info (0,0,&_radiodata, &_fanet_mac);
 			terminal_mac_info (0,0,&_radiodata, &_fanet_mac); printf ("\n");
 		}
-		
+
 		update_routing (&_radiodata, &_fanet_mac);
     }
 }
@@ -237,8 +237,8 @@ int main (int argc, char *argv[])
 	//char _minute_new;
 	//char _minute_old;
 
-    //sql_login();
-    
+    sql_login();
+
    // if (argc < 2) {
     //    printf ("Usage: argv[0] sender|rec [message]\n");
      //   exit(1);
@@ -248,21 +248,8 @@ int main (int argc, char *argv[])
 
     delay(2000);
 
-    if (argc > 0) {
-        printf ("-- %s \n", argv[1]);
-    }
-    if (argc > 1) {
-        printf ("-- %s \n", argv[2]);
-	send_mess(argv[1]);
-        printf ("--\n");
-	exit(0);
-    }
-
-
     //start_screen();
     terminal_start_screen(sf, 250, freq);
-
-    
 
     while(1)
     {
@@ -270,25 +257,25 @@ int main (int argc, char *argv[])
 		tm = localtime (&now);
 		_second_new = tm->tm_sec;
 		//_minute_new = tm->tm_min;
-		
+
 		system_data();
 		receivepacket();
-		
-		if (_second_new!=_second_old) 
+
+		if (_second_new!=_second_old)
 		{
 		  _second_old = _second_new;
-		  
+
 		  fanet_t4_service_scheduler();
 		  fanet_t3_messenger_scheduler();
-		
+
 		}
-		
+
 		/*if (_minute_new!=_minute_old)
 		{
 			_minute_old = _minute_new;
 			show_register();
 		}*/
-		
+
         delay(1);
 	}
     return (0);
