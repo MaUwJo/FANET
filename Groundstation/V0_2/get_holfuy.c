bc88dcc7868cfd3ef686fb9fbc9ea8540f548fff
mreinart <manfred@reinart.de>
2019-10-01 09:27:50 +0200

    Update get_holfuy.c
    URL pattern - Station-ID & PWD

--- Groundstation/V0_2/get_holfuy.c
+++ Groundstation/V0_2/get_holfuy.c
@@ -31,7 +31,7 @@
 
 #include "fanet_struct.h"
 
-const char *holfuyUrlFormat = "http://api.holfuy.com/live/?s=%s&pw=XXXXXXXXXXX>";
+const char *holfuyUrlFormat = "http://api.holfuy.com/live/?s=%s&pw=%s>";
 
 sWeather hf711_weather;
 sWeather hf795_weather;
